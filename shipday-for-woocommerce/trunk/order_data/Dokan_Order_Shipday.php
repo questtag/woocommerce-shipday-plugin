91eb06266c1df4dbfa856c2d0d88fb7eb8a07799
shahriar <shhrrtnvr@gmail.com>
2022-01-16 22:26:22 +0600

    bug_fix

--- shipday-for-woocommerce/trunk/order_data/Dokan_Order_Shipday.php
+++ shipday-for-woocommerce/trunk/order_data/Dokan_Order_Shipday.php
@@ -2,63 +2,50 @@
 require_once dirname( __DIR__ ) . '/functions/common.php';
 require_once dirname( __DIR__ ) . '/date-modifiers/order_delivery_date.php';
 
-class Dokan_Order_Shipday {
-	private $post, $sub_orders;
+class Dokan_Order_Shipday extends Woo_Order_Shipday {
+	protected $order, $vendor_id;
 
+    private $prevent_flag;
 	function __construct($post) {
         logger('info', 'Constructing Dokan order from order id '.$post);
+        if ($this->has_suborders($post)) {
+            $this->prevent_flag = true;
+        } else {
+            $this->prevent_flag = false;
+            $this->order = wc_get_order($post);
+            $this->vendor_id   = dokan_get_seller_id_by_order( $this->order );
+        }
+    }
 
-        $this->post = $post;
-	}
-
-    function get_dokan_api_key($vendor_id) {
+    function get_dokan_api_key() {
         if (get_order_manager() == 'admin_manage') return get_shipday_api_key();
-        $api_key            = get_user_meta( $vendor_id, 'shipday_api_key', true );
+        $api_key            = get_user_meta( $this->vendor_id, 'shipday_api_key', true );
         return handle_null($api_key);
     }
 
 	public function get_payloads() {
-		$this->create_suborders();
-		if ($this->sub_orders) {
-//			foreach ($this->sub_orders as $sub_order) {
-//				$sub_order = wc_get_order($sub_order);
-//				$vendor_id   = dokan_get_seller_id_by_order( $sub_order );
-//				$wc_order = wc_get_order($this->post);
-//                $api_key = $this->get_dokan_api_key($vendor_id);
-//                echo $api_key;
-//				$payloads[$api_key][] = array_merge(
-//					(new Woo_Order_Shipday($sub_order))->get_payload_without_dependant_info(),
-//					$this->get_vendor_info($vendor_id),
-//					get_times($wc_order),
-//					$this->get_signature($vendor_id)
-//				);
-//			}
-            $payloads = [];
-		} else {
-			$order = wc_get_order($this->post);
-			$vendor_id   = dokan_get_seller_id_by_order( $order );
-            $api_key = $this->get_dokan_api_key($vendor_id);
-            logger('INFO', 'Vendor Id:'.$vendor_id. ', API Key:'.$api_key);
-			$payloads[$api_key][] = array_merge(
-				(new Woo_Order_Shipday($order))->get_payload_without_dependant_info(),
-				$this->get_vendor_info($vendor_id),
-				get_times($order),
-				$this->get_signature($vendor_id)
-			);
-		}
+        $api_key = $this->get_dokan_api_key();
+        logger('INFO', 'Vendor Id:'.$this->vendor_id. ', API Key:'.$api_key);
+        $payloads[$api_key][] = array_merge(
+            $this->get_payload_without_dependant_info(),
+            $this->get_vendor_info(),
+            get_times($this->order),
+            $this->get_signature()
+        );
+
 		return $payloads;
 	}
 
-	public function create_suborders() {
-		$this->sub_orders = get_children(
+	public function has_suborders($post) {
+		return (bool) get_children(
 			array(
-				'post_parent' => $this->post,
+				'post_parent' => $post,
 				'post_type'   => 'shop_order',
 			)
 		);
 	}
-	public function get_vendor_info($vendor_id) {
-		$vendor = new \WeDevs\Dokan\Vendor\Vendor($vendor_id);
+	public function get_vendor_info() {
+		$vendor = new \WeDevs\Dokan\Vendor\Vendor($this->vendor_id);
 		$pickup_store = handle_null($vendor->get_shop_name());
 		$address = handle_null(implode(', ', $vendor->get_address()));
 		$phone = handle_null($vendor->get_phone());
@@ -70,7 +57,7 @@ class Dokan_Order_Shipday {
 		);
 	}
 
-	function get_signature($store_id): array {
+	function get_signature(): array {
 		global $shipday_plugin_version;
 		return array(
 			'orderSource' => 'woocommerce',
@@ -78,7 +65,7 @@ class Dokan_Order_Shipday {
 				'version' => $shipday_plugin_version,
 				'wooVersion' => WC()->version,
 				'type' => 'multi-vendor',
-				'vendorId' => $store_id,
+				'vendorId' => $this->vendor_id,
 				'plugin' => 'Dokan',
 				'dokanVersion' => dokan()->version,
                 'orderManagedBy' => get_order_manager(),
@@ -86,4 +73,8 @@ class Dokan_Order_Shipday {
 			)
 		);
 	}
+
+    public function prevent_order_sync() {
+        return $this->prevent_flag | parent::prevent_order_sync();
+    }
 }
