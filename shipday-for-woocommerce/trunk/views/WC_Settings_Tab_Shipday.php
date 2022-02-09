c040225932c84ab742364ad1e5fc8bc4779a6757
shahriar <shhrrtnvr@gmail.com>
2022-02-10 02:37:26 +0600

    1.0.17 legacy code removed

--- shipday-for-woocommerce/trunk/views/WC_Settings_Tab_Shipday.php
+++ shipday-for-woocommerce/trunk/views/WC_Settings_Tab_Shipday.php
@@ -8,20 +8,12 @@ class WC_Settings_Tab_Shipday
 		add_filter('woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 50);
 		add_action('woocommerce_settings_tabs_settings_tab_shipday', __CLASS__ . '::settings_tab');
 		add_action('woocommerce_update_options_settings_tab_shipday', __CLASS__ . '::update_settings');
-		add_action('admin_enqueue_scripts', __CLASS__ . '::register_wcsscript'); // admin enqueue for registered js file.
-	}
-
-	public static function register_wcsscript()
-	{
-		wp_enqueue_script('custom-wcsscript', plugin_dir_url(__FILE__) . 'js/wc-shipday-script.js', array(), '1.0');
-		wp_enqueue_script('custom-wcsscript');
 	}
 
 	public static function add_settings_tab($settings_tabs)
 	{
 		$settings_tabs['settings_tab_shipday'] = __('Shipday', 'woocommerce-settings-tab-shipday');
 		return $settings_tabs;
-
 	}
 
 	public static function settings_tab()
